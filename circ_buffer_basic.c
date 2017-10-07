3d9468e5b86d4eec7ce0b58852d31ef14ab0b76a
LGKev <kevinkuwata@icloud.com>
2017-10-07 10:33:39 -0600

    so it appears memory alloc can be done with a regular pointer.and not a circBuf_t pointer. I can see the values being added and removed properly on a small 2 unit test

--- circ_buffer_basic.c
+++ circ_buffer_basic.c
@@ -5,6 +5,7 @@
  *      Author: kk
  */
 
+
 #include "circular_buffer.h"
 #include <stdlib.h>
 #include "lab3.h"
@@ -12,14 +13,14 @@
 
 
 void initBuffBasic(CircBuf_t **myBase, uint8_t _length){
-  CircBuf_t  *buffer = (CircBuf_t * )malloc(_length*sizeof(uint8_t));
+  uint8_t  *buffer = (uint8_t * )malloc(_length*sizeof(uint8_t));
 
           (*myBase) ->num_items =0;
           (*myBase) ->tailPosition = 0;
           (*myBase) ->length = _length;
-          (*myBase) ->head = &buffer;
-          (*myBase) ->tail = &buffer;
-          (*myBase) ->buffer = &buffer;
+          (*myBase) ->head = buffer;
+          (*myBase) ->tail = buffer;
+          (*myBase) ->buffer = buffer;
 
 
 
