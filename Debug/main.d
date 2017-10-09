1393c62393776ee9a681578bed4fb33c573d92cc
LGKev <kevinkuwata@icloud.com>
2017-10-08 23:05:16 -0600

    word counting works

---
+++ Debug/main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/msp.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/msp432p401r.h
+main.obj: /home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: /home/kk/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+main.obj: ../uart.h
+main.obj: /home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdlib.h
+main.obj: /home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/linkage.h
+main.obj: /home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/string.h
+main.obj: ../circular_buffer.h
+main.obj: ../lab3.h
+main.obj: ../circ_buffer_basic.h
+main.obj: ../circular_buffer.h
+
+../main.c: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/msp.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+/home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/home/kk/ti/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
+../uart.h: 
+/home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdlib.h: 
+/home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/linkage.h: 
+/home/kk/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/string.h: 
+../circular_buffer.h: 
+../lab3.h: 
+../circ_buffer_basic.h: 
+../circular_buffer.h: 
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include "uart.h"
 #include <stdlib.h>
+#include <string.h>
 
 #include "circular_buffer.h"
 #include "lab3.h"
@@ -13,138 +14,156 @@
  * main.c
  */
 
- char  myShortTest[9]={"Test123\n"};
+char myShortTest[9] = { "Test123\n" };
 
-  CircBuf_t myBuffer;
-  CircBuf_t * myBufferPTR = &myBuffer;
+CircBuf_t myBuffer;
+CircBuf_t * myBufferPTR = &myBuffer;
 
+uint8_t zoid[17] = { 87, 104, 121, 32, 78, 111, 116, 32, 90, 111, 105, 100, 98,
+                     101, 114, 103, 63 };
 
-  uint8_t zoid[17] = {87,104,121,32,78,111,116,32,90,111,105,100,98,101,114,103,63};
+uint8_t Calculate_Stats = 0; // set to 1, either full or enter pressed.
 
-  uint8_t Calculate_Stats = 0; // set to 1, either full or enter pressed.
-
-
-  uint8_t count[5]={0,0,0,0,0};
+uint8_t count[5] = { 0, 0, 0, 0, 0 };
 void create_Test_Buffer();
-
 
 void main(void)
 {
-
-
-
-
 
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
     UART_config();
     configure_clocks(); //probably so change up
     configure_ports();
 
-
     initialize_Circ_Buffer(&myBufferPTR, 200);
 
-
-
     __enable_irq();
-
 
     P2SEL0 &= ~BLUE_LED;
     P2SEL1 &= ~BLUE_LED;
     P2DIR |= BLUE_LED; //output
     P2OUT &= ~BLUE_LED;
 
-
 #ifdef UART_TX_TEST
     uint8_t letter = 0;
-	while(1){
-	   for(letter = 30; letter < 122; letter++){
-	    UART_putchar(letter);
-	    UART_putchar(32);//space
-	   }
+    while(1)
+    {
+        for(letter = 30; letter < 122; letter++)
+        {
+            UART_putchar(letter);
+            UART_putchar(32); //space
+        }
 
-	    }
+    }
 #endif
-
 
 #ifdef ZOIDBERG_TEST
-	uint8_t letter = 0;
-	while(1){
-	    uart_putchar_n("Why Not ZoidBerg", 17);
-	    UART_putchar(13);
-	}
+    uint8_t letter = 0;
+    while(1)
+    {
+        uart_putchar_n("Why Not ZoidBerg", 17);
+        UART_putchar(13);
+    }
 #endif
 
-	/*stats*/
+    /*stats*/
 
-while(1){
-	if(Calculate_Stats == 1){
+    while (1)
+    {
+        if (Calculate_Stats == 1)
+        {
 
-	    uint8_t data =0;
-	    uint8_t count_letter =0;
-	    uint8_t count_number = 0;
-	    uint8_t count_puntuation=0;
-	    uint8_t count_white_space =0;
-	    uint8_t count_other_character =0;
-	    uint8_t i;
+            uint8_t data = 0;
+            uint8_t count_letter = 0;
+            uint8_t count_number = 0;
+            uint8_t count_puntuation = 0;
+            uint8_t count_white_space = 0;
+            uint8_t count_other_character = 0;
+            uint8_t i;
 
-	   print(myBufferPTR);
-	   UART_putchar(13);
+            print(myBufferPTR);
+            UART_putchar(13);
 
-	    uint8_t number_of_item = myBufferPTR -> num_items;
-	     for ( i=0; i< number_of_item; i++){
-	         data = remove_From_Buffer(&myBufferPTR);
-	         if(64<data & data<91 | 96<data & data <123){
-	             count_letter ++;
-	         }
-	             else if (47<data & data<58){
-	                 count_number ++;
-	             }
-	             else if (32<data & data<35|38<data& data<42|data ==46|57<data& data<60|data == 63){
-	                 count_puntuation ++;
-	                 }
-	             else if (data == 32){ ////TODO: horizontal tab too
-	                 count_white_space ++;
-	             }
-	             else
-	             {
-	                count_other_character++;
-	             }
-	     }
-	     count[0] = count_letter + Ascii_num_offset;
-	     count[1]= count_number+ Ascii_num_offset;
-	     count[2]= count_puntuation+ Ascii_num_offset;
-	     count[3] = count_white_space+ Ascii_num_offset;
-	     count[4]= count_other_character+ Ascii_num_offset;
+            uint8_t number_of_item = myBufferPTR->num_items;
+            for (i = 0; i < number_of_item; i++)
+            {
+                data = remove_From_Buffer(&myBufferPTR);
+                if (64 < data & data < 91 | 96 < data & data < 123)
+                {
+                    count_letter++;
+                }
+                else if (47 < data & data < 58)
+                {
+                    count_number++;
+                }
+                else if (32 < data & data < 35 | 38 < data & data < 42
+                        | data == 46 | 57 < data & data < 60 | data == 63)
+                {
+                    count_puntuation++;
+                }
+                else if (data == 32)
+                { ////TODO: horizontal tab too
+                    count_white_space++;
+                }
+                else
+                {
+                    count_other_character++;
+                }
+            }
+            count[0] = count_letter+Ascii_num_offset;
+            count[1] = count_number + Ascii_num_offset;
+            count[2] = count_puntuation + Ascii_num_offset;
+            count[3] = count_white_space + Ascii_num_offset;
+            count[4] = count_other_character + Ascii_num_offset;
 
-	     uint8_t index =0;
+            uint8_t index = 0;
+
+            uint8_t ascii_letter[10];
+            uint8_t ascii_number[10];
+            uint8_t ascii_punctuation[10];
+            uint8_t ascii_space[10];
+            uint8_t ascii_other[10];
+
+            itoa(count_letter, ascii_letter, 10);
+            itoa(count_number, ascii_number, 10);
+            itoa(count_puntuation, ascii_punctuation, 10);
+            itoa(count_white_space, ascii_space, 10);
+            itoa(count_other_character, ascii_other, 10);
+
+            uart_putchar_n("Number of Letters:  ", strlen("Number of Letters:  "));
+            uart_putchar_n(ascii_letter, strlen(ascii_letter)); //letter
+            UART_putchar(13);
+
+            uart_putchar_n("Number of Numbers:  ", strlen("Number of Numbers:  "));
+            uart_putchar_n(ascii_number, strlen(ascii_number)); //letter
+            UART_putchar(13);
+
+            uart_putchar_n("Number of Punctuation:  ", strlen("Number of Punctuation:  "));
+            uart_putchar_n(ascii_punctuation, strlen(ascii_punctuation)); //letter
+            UART_putchar(13);
+
+            uart_putchar_n("Number of White Spaces:  ", strlen("Number of White Spaces:  "));
+            uart_putchar_n(ascii_space, strlen(ascii_space)); //letter
+            UART_putchar(13);
+
+            uart_putchar_n("Number of other characters:  ", strlen("Number of other characters:  "));
+            uart_putchar_n(ascii_other, strlen(ascii_other)); //letter
+            UART_putchar(13);
 
 
 
-	         UART_putchar(count[0]); //letter
-	         UART_putchar(count[1]); //number
-	         UART_putchar(count[2]); //punctuation
-	         UART_putchar(count[3]);  //white space
-	         UART_putchar(count[4]);   //others
+            (myBufferPTR)->tail = (myBufferPTR)->baseConst;
+            (myBufferPTR)->head = (myBufferPTR)->baseConst;
+            (myBufferPTR)->tailPosition = 0;
 
+            Calculate_Stats = 0;
+        }
+    }
 
-
-	     (myBufferPTR)->tail = (myBufferPTR)->baseConst;
-	     (myBufferPTR)->head = (myBufferPTR)->baseConst;
-	     (myBufferPTR)->tailPosition = 0;
-
-
-
-	     Calculate_Stats = 0;
-	}
 }
 
-
-
-
-}
-
-
-void HSMCLK_OUT(){
+void HSMCLK_OUT()
+{
 
     //primary mode, clock output HSMCLK
     //page 10.2.6
@@ -153,14 +172,14 @@ void HSMCLK_OUT(){
 
     P4DIR |= BIT4;
 
-
 }
 
-
-void create_Test_Buffer(){
+void create_Test_Buffer()
+{
     uint8_t array[30];
-    uint8_t index =0;
-    for(index=0; index< 30; index++){
+    uint8_t index = 0;
+    for (index = 0; index < 30; index++)
+    {
         add_To_Buffer(&myBufferPTR, index);
     }
 }
